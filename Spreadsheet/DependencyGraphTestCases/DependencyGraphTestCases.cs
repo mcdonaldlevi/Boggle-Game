47decee2ba5689c3f08614e8bd2498933f5ea785
RubioJr9 <tarkinater@gmail.com>
2017-02-02 19:13:09 -0700

    -ignore

---
+++ Spreadsheet/DependencyGraphTestCases/DependencyGraphTestCases.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DependencyGraphTestCases
+{
+    [TestClass]
+    public class DependencyGraphTestCases
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        /// <summary>
+        /// This tests that a syntactically incorrect parameter to Formula results
+        /// in a FormulaFormatException.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct1()
+        {
+            Formula f = new Formula("_");
+        }
+    }
+}
