f6f857d295fc1128ac9b6ef36d2f7d6f20722a4a
mcdonaldlevi <mcdonaldlevi@ymail.com>
2017-03-28 13:50:05 -0600

    Built Player, JoinGameInfo, GameInfo, andUserInfo classes. Built CreateUser, and JoinGame methods.

--- BoggleService/BoggleService/BoggleService.svc.cs
+++ BoggleService/BoggleService/BoggleService.svc.cs
@@ -3,12 +3,47 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.ServiceModel.Web;
+using System.Threading;
 using static System.Net.HttpStatusCode;
 
 namespace Boggle
 {
+    public class UserInfo
+    {
+        public string Name { get; set; }
+    }
+    public class JoinGameInfo
+    {
+        public string UserToken { get; set; }
+        public int TimeLimit { get; set; }
+    }
+    public class GameInfo
+    {
+        public string GameState { get; set; }
+        public string Board { get; set; }
+        public int TimeLimit { get; set; }
+        public int TimeLeft { get; set; }
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+    }
+    public class Player
+    {
+        public string UserToken { get; set; }
+        public string Nickname { get;set; }
+        public int Score { get; set; }
+        public WordPlayed WordsPlayed { get; set; }
+    }
+    public class WordPlayed
+    {
+        public string Word { get; set; }
+        public int Score { get; set; }
+    }
     public class BoggleService : IBoggleService
     {
+        private static Dictionary<String, GameInfo> games = new Dictionary<string, GameInfo>();
+        private  static Dictionary<String, UserInfo> users = new Dictionary<String, UserInfo>();
+        private static readonly object sync = new object();
+        private GameInfo pendingGame = null;
         /// <summary>
         /// The most recent call to SetStatus determines the response code used when
         /// an http response is sent.
@@ -60,6 +95,60 @@ namespace Boggle
             {
                 SetStatus(Forbidden);
                 return null;
+            }
+        }
+        public string CreateUser(UserInfo user)
+        {
+            lock (sync)
+            {
+                if (user.Name == "stall")
+                {
+                    Thread.Sleep(5000);
+                }
+                if (user.Name == null || user.Name.Trim().Length == 0)
+                {
+                    SetStatus(Forbidden);
+                    return null;
+                }
+                else
+                {
+                    string userID = Guid.NewGuid().ToString();
+                    users.Add(userID, user);
+                    SetStatus(Created);
+                    return userID;
+                }
+            }
+        }
+        public string JoinGame(JoinGameInfo user)
+        {
+            lock (sync)
+            {
+                if (user.UserToken == null || user.UserToken.Trim().Length == 0)
+                {
+                    SetStatus(Forbidden);
+                    return null;
+                }
+                
+                if(pendingGame == null)
+                {
+                    pendingGame.Player1.UserToken = user.UserToken;
+                    pendingGame.TimeLimit = user.TimeLimit;
+                    pendingGame.GameState = "pending";
+                    BoggleBoard board = new BoggleBoard();
+                    pendingGame.Board = board.
+                }
+                if (pendingGame != null && pendingGame.Player1.UserToken == user.UserToken)
+                {
+                    SetStatus(Conflict);
+                    return null;
+                }
+                else
+                {
+                    string userID = Guid.NewGuid().ToString();
+                    users.Add(userID, user);
+                    SetStatus(Created);
+                    return userID;
+                }
             }
         }
     }
