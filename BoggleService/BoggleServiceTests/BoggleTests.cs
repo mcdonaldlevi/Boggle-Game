79502aab35d5b45cab8462150cf52d03d73429ac
Slater <Slater@DESKTOP-PR8GLA3>
2017-03-28 18:15:14 -0600

    -added more test cases -changed NickName to Nickname to better follow API

--- BoggleService/BoggleServiceTests/BoggleTests.cs
+++ BoggleService/BoggleServiceTests/BoggleTests.cs
@@ -92,7 +92,7 @@ namespace Boggle
         public void CreateUserTest1()
         {
             dynamic file = new ExpandoObject();
-            file.NickName = "Sally";
+            file.Nickname = "Sally";
             Response r = client.DoPostAsync("users", file).Result;
             Assert.AreEqual(Created, r.Status);
             Assert.AreEqual(r.Data.Length, 36);
@@ -105,7 +105,7 @@ namespace Boggle
         public void CreateUserTest2()
         {
             dynamic file = new ExpandoObject();
-            file.NickName = "Sebastian";
+            file.Nickname = "Sebastian";
             Response r1 = client.DoPostAsync("users", file).Result;
             Response r2 = client.DoPostAsync("users", file).Result;
             Assert.AreEqual(Created, r1.Status);
@@ -120,7 +120,7 @@ namespace Boggle
         public void CreateUserTest3()
         {
             dynamic file = new ExpandoObject();
-            file.NickName = "";
+            file.Nickname = "";
             Response r = client.DoPostAsync("users", file).Result;
             Assert.AreEqual(Forbidden, r.Status);
 
@@ -134,7 +134,7 @@ namespace Boggle
         public void CreateUserTest4()
         {
             dynamic file = new ExpandoObject();
-            file.NickName = null;
+            file.Nickname = null;
             Response r = client.DoPostAsync("users", file).Result;
             Assert.AreEqual(Forbidden, r.Status);
         }
@@ -146,22 +146,109 @@ namespace Boggle
         public void CreateUserTest5()
         {
             dynamic file = new ExpandoObject();
-            file.NickName = "     ";
+            file.Nickname = "     ";
             Response r = client.DoPostAsync("users", file).Result;
             Assert.AreEqual(Forbidden, r.Status);
         }
 
+        /// <summary>
+        /// Two users are created
+        /// When joining a game, first user gets an accepted status
+        /// When joining a game, second user gets a created status
+        /// </summary>
         [TestMethod]
         public void JoinGameTest1()
         {
+            //Creates first user
+            dynamic user1 = new ExpandoObject();
+            user1.Nickname = "Kanye";
+            Response p1_token = client.DoPostAsync("users", user1).Result;
+
+            //Creates second user
+            dynamic user2 = new ExpandoObject();
+            user2.Nickname = "Ajay";
+            Response p2_token = client.DoPostAsync("users", user2).Result;
+
+            //Player 1 joins game
             dynamic file1 = new ExpandoObject();
-            file1.NickName = "Kanye";
-            Response r = client.DoPostAsync("users", file1).Result;
+            file1.UserToken = p1_token.Data;
+            file1.TimeLimit = 30;
+            Response r1 = client.DoPostAsync("games", file1).Result;
+            Assert.AreEqual(Accepted, r1.Status);
+
+            //Player 2 joins game
             dynamic file2 = new ExpandoObject();
-            file2.UserToken = r.Data;
+            file2.UserToken = p2_token.Data;
             file2.TimeLimit = 30;
-            r = client.DoPostAsync("games", file2).Result;
-            Assert.AreEqual(Accepted, r.Status);
+            Response r2 = client.DoPostAsync("games", file2).Result;
+            Assert.AreEqual(Created, r2.Status);
+
+            //Asserts that the two players have the same game id
+            Assert.AreEqual(r1.Data, r2.Data);
+        }
+
+        /// <summary>
+        /// User is created and attempts to join with an invalid token
+        /// </summary>
+        [TestMethod]
+        public void JoinGameTest2()
+        {
+            dynamic user1 = new ExpandoObject();
+            user1.Nickname = "James";
+            Response p1_token = client.DoPostAsync("users", user1).Result;
+
+            dynamic file1 = new ExpandoObject();
+            file1.UserToken = "this token iz legit lulz";
+            file1.TimeLimit = 30;
+            Response r1 = client.DoPostAsync("games", file1).Result;
+            Assert.AreEqual(Forbidden, r1.Status);
+        }
+
+        /// <summary>
+        /// if UserToken is already a player in the pending game, responds with status conflict
+        /// </summary>
+        [TestMethod]
+        public void JoinGameTest3()
+        {
+            //Creates user
+            dynamic user1 = new ExpandoObject();
+            user1.Nickname = "Billy";
+            Response r1 = client.DoPostAsync("users", user1).Result;
+
+            dynamic file1 = new ExpandoObject();
+            file1.UserToken = r1.Data;
+            file1.TimeLimit = 60;
+
+            //Sends the same user token twice
+            Response r2 = client.DoPostAsync("games", file1).Result;
+            Assert.AreEqual(Accepted, r2.Status);
+
+            Response r3 = client.DoPostAsync("games", file1).Result;
+            Assert.AreEqual(Conflict, r3.Status);
+        }
+
+        /// <summary>
+        /// Cancels a game after requesting a join
+        /// </summary>
+        [TestMethod]
+        public void CancelJoinRequestTest1()
+        {
+            //Creates user
+            dynamic user1 = new ExpandoObject();
+            user1.Nickname = "Jon";
+            Response r1 = client.DoPostAsync("users", user1).Result;
+
+            //Joins game
+            dynamic file1 = new ExpandoObject();
+            file1.UserToken = r1.Data;
+            file1.TimeLimit = 60;
+            client.DoPostAsync("games", file1);
+
+            //Sends cancel request
+            dynamic file2 = new ExpandoObject();
+            file2.UserToken = r1.Data;
+            Response r2 = client.DoPutAsync("games", file2).Result;
+            Assert.AreEqual(OK, r2.Status);
         }
     }
 }
