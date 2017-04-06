fbef131826932999a44abdcb58c9176f629fc30c
Slater <Slater@DESKTOP-PR8GLA3>
2017-04-06 01:53:19 -0600

    -added to JoinGame

--- BoggleService/BoggleService/BoggleService.svc.cs
+++ BoggleService/BoggleService/BoggleService.svc.cs
@@ -138,9 +138,42 @@ namespace Boggle
                                 }
                             }
                         }
-                        using (SqlCommand command = new SqlCommand("insert into Table (UserID, "))
+
+
+                        if (pendingGame.GameState == "inactive")
+                        {
+                            using (SqlCommand command = new SqlCommand("insert into Table (GameID, TimeLimit) values(@GameID, @TimeLimit)", conn, trans))
+                            {
+                                string gameID = Guid.NewGuid().ToString();
+                                command.Parameters.AddWithValue("@GameID", gameID);
+                                command.Parameters.AddWithValue("@TimeLimit", user.TimeLimit * 1000);
+
+                                SetStatus(Accepted);
+                                pendingGame = new GameInfo { GameState = "pending" };
+
+                                trans.Commit();
+                                return new GameIDInfo { GameID = gameID };
+                            }
+                        }
+                        else if (pendingGame.GameState == "pending")
+                        {
+                            using (SqlCommand command = new SqlCommand("insert into Table (TimeLimit) values(@TimeLimit)", conn, trans))
+                            {
+                                string gameID = new SqlCommand("select 'GameID' from Table", conn, trans).ExecuteReader().ToString();
+                                string player1TimeLimit = new SqlCommand("select 'TimeLimit' from Table", conn, trans).ExecuteReader().ToString();
+                                int newTimeLimit = (Int32.Parse(player1TimeLimit) + user.TimeLimit) / 2;
+                                command.Parameters.AddWithValue("@TimeLimit", newTimeLimit);
+
+                                SetStatus(Created);
+                                pendingGame = new GameInfo { GameState = "inactive" };
+
+                                trans.Commit();
+                                return new GameIDInfo { GameID = gameID };
+                            }
+                        }
                     }
                 }
+
 
                     if (pendingGame.GameState == "inactive")
                     {
